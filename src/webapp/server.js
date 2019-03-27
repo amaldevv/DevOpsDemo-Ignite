7b8f56623c54313340425176251209d695dae32c
Amal <amaldevv@outlook.com>
2019-03-27 05:05:13 +0400

    Added inital code

---
+++ src/webapp/server.js
@@ -0,0 +1,32 @@
+'use strict';
+var os = require('os');
+const express = require('express');
+const request = require('request');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.static("public"));
+
+app.get('/index', (req, res) => {
+  res.send(__dirname + "/" + "index.html");
+});
+// api ------------------------------------------------------------
+app.get('/api/getMessage', function (req, res) {
+  
+
+  
+
+  // Invoke service
+  request('http://web-api:9900/api/values', function (error, response, body) {
+      if(error) console.log(error);
+      res.send('Hello from WebAPP running on ' + os.hostname() + ' and ' + body);
+      console.log('Hello from WebAPI running on ' + os.hostname() + ' and ' + body);
+  });
+}); 
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
